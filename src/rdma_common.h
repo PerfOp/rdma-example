06853190978aa5396f53c81552a1c6099dfeffe7
huajianwang <huajianwang@microsoft.com>
2025-10-15 02:29:05 +0000

    MOD: Server structure.

--- src/rdma_common.h
+++ src/rdma_common.h
@@ -10,32 +10,31 @@
 #ifndef RDMA_COMMON_H
 #define RDMA_COMMON_H
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <arpa/inet.h>
 #include <errno.h>
 #include <getopt.h>
-
+#include <infiniband/verbs.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
 #include <rdma/rdma_cma.h>
-#include <infiniband/verbs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 /* Error Macro*/
 #define rdma_error(msg, args...)
-//do {fprintf(stderr, "%s : %d : ERROR : %s\n", __FILE__, __LINE__, msg, ##__VA_ARGS__);}while(0);
-	// fprintf(stderr, "%s : %d : ERROR : "msg, __FILE__, __LINE__, ## args);\
+// do {fprintf(stderr, "%s : %d : ERROR : %s\n", __FILE__, __LINE__, msg,
+// ##__VA_ARGS__);}while(0);
+//  fprintf(stderr, "%s : %d : ERROR : "msg, __FILE__, __LINE__, ## args);\
 
 #ifdef ACN_RDMA_DEBUG
 /* Debug Macro */
-#define debug(msg, args...) do {\
-    printf("DEBUG: "msg, ## args);\
-}while(0);
+#define debug(msg, args...)            \
+    do {                               \
+        printf("DEBUG: " msg, ##args); \
+    } while (0);
 
 #else
 
@@ -53,20 +52,21 @@
 #define DEFAULT_RDMA_PORT (20886)
 
 /*
- * We use attribute so that compiler does not step in and try to pad the structure.
- * We use this structure to exchange information between the server and the client.
+ * We use attribute so that compiler does not step in and try to pad the
+ * structure. We use this structure to exchange information between the server
+ * and the client.
  *
  * For details see: http://gcc.gnu.org/onlinedocs/gcc/Type-Attributes.html
  */
 struct __attribute((packed)) rdma_buffer_attr {
-  uint64_t address;
-  uint32_t length;
-  union stag {
-	  /* if we send, we call it local stags */
-	  uint32_t local_stag;
-	  /* if we receive, we call it remote stag */
-	  uint32_t remote_stag;
-  }stag;
+    uint64_t address;
+    uint32_t length;
+    union stag {
+        /* if we send, we call it local stags */
+        uint32_t local_stag;
+        /* if we receive, we call it remote stag */
+        uint32_t remote_stag;
+    } stag;
 };
 /* resolves a given destination name to sin_addr */
 int get_addr(char *dst, struct sockaddr *addr);
@@ -81,19 +81,19 @@ void show_rdma_buffer_attr(struct rdma_buffer_attr *attr);
  * @cm_event: where the event will be stored
  */
 int process_rdma_cm_event(struct rdma_event_channel *echannel,
-		enum rdma_cm_event_type expected_event,
-		struct rdma_cm_event **cm_event);
+                          enum rdma_cm_event_type expected_event,
+                          struct rdma_cm_event **cm_event);
 
 /* Allocates an RDMA buffer of size 'length' with permission permission. This
  * function will also register the memory and returns a memory region (MR)
  * identifier or NULL on error.
  * @pd: Protection domain where the buffer should be allocated
  * @length: Length of the buffer
- * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
+ * @permission: OR of IBV_ACCESS_* permissions as defined for the enum
+ * ibv_access_flags
  */
-struct ibv_mr* rdma_buffer_alloc(struct ibv_pd *pd,
-		uint32_t length,
-		enum ibv_access_flags permission);
+struct ibv_mr *rdma_buffer_alloc(struct ibv_pd *pd, uint32_t length,
+                                 enum ibv_access_flags permission);
 
 /* Frees a previously allocated RDMA buffer. The buffer must be allocated by
  * calling rdma_buffer_alloc();
@@ -101,33 +101,67 @@ struct ibv_mr* rdma_buffer_alloc(struct ibv_pd *pd,
  */
 void rdma_buffer_free(struct ibv_mr *mr);
 
-/* This function registers a previously allocated memory. Returns a memory region
- * (MR) identifier or NULL on error.
+/* This function registers a previously allocated memory. Returns a memory
+ * region (MR) identifier or NULL on error.
  * @pd: protection domain where to register memory
  * @addr: Buffer address
  * @length: Length of the buffer
- * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
+ * @permission: OR of IBV_ACCESS_* permissions as defined for the enum
+ * ibv_access_flags
  */
-struct ibv_mr *rdma_buffer_register(struct ibv_pd *pd,
-		void *addr,
-		uint32_t length,
-		enum ibv_access_flags permission);
+struct ibv_mr *rdma_buffer_register(struct ibv_pd *pd, void *addr,
+                                    uint32_t length,
+                                    enum ibv_access_flags permission);
 /* Deregisters a previously register memory
  * @mr: Memory region to deregister
  */
 void rdma_buffer_deregister(struct ibv_mr *mr);
 
 /* Processes a work completion (WC) notification.
- * @comp_channel: Completion channel where the notifications are expected to arrive
+ * @comp_channel: Completion channel where the notifications are expected to
+ * arrive
  * @wc: Array where to hold the work completion elements
  * @max_wc: Maximum number of expected work completion (WC) elements. wc must be
  *          atleast this size.
  */
 int process_work_completion_events(struct ibv_comp_channel *comp_channel,
-		struct ibv_wc *wc,
-		int max_wc);
+                                   struct ibv_wc *wc, int max_wc);
 
 /* prints some details from the cm id */
 void show_rdma_cmid(struct rdma_cm_id *id);
 
+class RdmaServer {
+public:
+    struct rdma_event_channel *cm_event_channel{NULL};
+    struct rdma_cm_id *cm_server_id{NULL};
+    struct ibv_pd *pd{NULL};
+    struct ibv_cq *cq{NULL};
+    struct ibv_comp_channel *io_completion_channel{NULL};
+    struct rdma_cm_id *cm_client_id{NULL};
+    struct ibv_qp *client_qp{NULL};
+    struct ibv_mr *client_metadata_mr{NULL};
+    struct ibv_mr *server_buffer_mr{NULL};
+    struct ibv_mr *server_metadata_mr{NULL};
+    struct ibv_recv_wr *bad_client_recv_wr{NULL};
+    struct ibv_send_wr *bad_server_send_wr{NULL};
+
+    // Variables
+    struct ibv_recv_wr client_recv_wr;
+    struct ibv_send_wr server_send_wr;
+    struct ibv_qp_init_attr qp_init_attr;
+    struct rdma_buffer_attr client_metadata_attr, server_metadata_attr;
+    struct ibv_sge client_recv_sge, server_send_sge;
+public:
+    int start_rdma_server(struct sockaddr_in *server_addr);
+    int setup_client_resources();
+    int accept_client_connection();
+    int send_server_metadata_to_client();
+    int disconnect_and_cleanup();
+};
+
+typedef struct _RdmaClient {
+    struct rdma_event_channel *cm_event_channel{NULL};
+    struct rdma_cm_id *cm_server_id{NULL};
+    struct ibv_pd *pd{NULL};
+} RdmaClient;
 #endif /* RDMA_COMMON_H */
